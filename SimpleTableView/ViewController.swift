7128abbf07535503fc24c50299ecb1296d53c6c0
Sudeb Sarkar <Sudeb@Sudebs-MacBook-Air.local>
2017-06-05 14:05:33 +0530

    Simple tableview 2

--- SimpleTableView/ViewController.swift
+++ SimpleTableView/ViewController.swift
@@ -8,7 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController ,UITableViewDataSource , UITableViewDelegate {
+class ViewController : UIViewController ,UITableViewDataSource , UITableViewDelegate {
+    @IBOutlet weak var tableview: UITableView!
+    
+    
+   
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +24,24 @@ class ViewController: UIViewController ,UITableViewDataSource , UITableViewDeleg
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell:UITableViewCell=UITableViewCell(style: UITableViewCellStyle.default,reuseIdentifier: "mycell")
+        cell.textLabel?.text="Sudeb Sarkar"
+//        cell.detailTextLabel?.text=" jhdkjfhsdfs ksfkhfhs fhskfks hfksd fs dksfdkskfsdhsd hsubtitle#\(indexPath.row)"
+        
+        return cell
+    }
+    
+    
+    func tableView(_ tableView:UITableView, numberOfRowsInSection section:Int) -> Int
+    {
+        return 5
+    }
 
-
+    
+    
 }
 
